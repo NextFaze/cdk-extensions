b278a5984a7ff20ba498d06b1299e009ce672f18
Rushi Patel <rp970.patel@gmail.com>
2020-08-27 00:12:38 +0930

    feat(web-application): add dynamic config parameter

---
+++ packages/web-application/facades/create-dynamic-config-parameter.ts
@@ -0,0 +1,24 @@
+import { WebApplication } from '../web-application.construct';
+import {
+  StringParameter,
+  ParameterTier,
+  ParameterType,
+} from '@aws-cdk/aws-ssm';
+
+export function createDynamicConfigParameter(
+  this: WebApplication
+): StringParameter | undefined {
+  const { dynamicParameter, aliases } = this.props;
+  if (!dynamicParameter) {
+    return;
+  }
+
+  return new StringParameter(this, 'ConfigParameter', {
+    stringValue: dynamicParameter.initialValue,
+    tier: ParameterTier.INTELLIGENT_TIERING,
+    description: `Dynamic Config Parameter for ${aliases[0]}`,
+    allowedPattern: dynamicParameter.allowedPattern,
+    type: ParameterType.STRING,
+    parameterName: dynamicParameter.parameterName,
+  });
+}
--- packages/web-application/web-application.construct.ts
+++ packages/web-application/web-application.construct.ts
@@ -1,3 +1,4 @@
+import { StringParameter } from '@aws-cdk/aws-ssm';
 import { Construct } from '@aws-cdk/core';
 import { IHostedZone } from '@aws-cdk/aws-route53';
 import { PriceClass } from '@aws-cdk/aws-cloudfront';
@@ -8,6 +9,7 @@ import { createOriginBucket } from './facades/create-origin-bucket';
 import { createCloudfrontWebDistribution } from './facades/create-cloudfront-web-distribution';
 import { getViewerCertificate } from './facades/get-viewer-certificate';
 import { addCnameRecords } from './facades/add-cname-records';
+import { createDynamicConfigParameter } from './facades/create-dynamic-config-parameter';
 
 export interface IRequestCertificateProps {
   domainName: string;
@@ -21,9 +23,15 @@ export interface IWebApplicationProps {
   defaultRootObject?: string;
   errorRootObject?: string;
   cloudfrontPriceClass?: PriceClass;
+  dynamicParameter?: {
+    parameterName?: string;
+    initialValue: string;
+    allowedPattern?: string;
+  };
 }
 
 export class WebApplication extends Construct {
+  configParameter: StringParameter | undefined;
   constructor(
     scope: Construct,
     id: string,
@@ -33,13 +41,16 @@ export class WebApplication extends Construct {
 
     const originBucket = createOriginBucket.call(this);
     const identity = createOriginAccessIdentity.call(this);
-    const viewerCertificate = getViewerCertificate.call(this);
+    originBucket.grantRead(identity);
 
+    const viewerCertificate = getViewerCertificate.call(this);
     const distribution = createCloudfrontWebDistribution.call(this, {
       s3BucketSource: originBucket,
       originAccessIdentity: identity,
       viewerCertificate,
     });
+
+    this.configParameter = createDynamicConfigParameter.call(this);
 
     addCnameRecords.call(this, { distribution });
   }
