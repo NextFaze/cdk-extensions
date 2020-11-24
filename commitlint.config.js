d52e323bf61e48c7303c586c8939ddc10ee1ed9d
whimzyLive <rp970.patel@gmail.com>
2020-11-24 18:10:40 +1030

    chore: enforce package names as scopes

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,16 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+const { readdirSync } = require('fs');
+
+const packages = () =>
+  readdirSync('./packages', { withFileTypes: true })
+    .filter((dir) => dir.isDirectory())
+    .map((dir) => dir.name);
+
+console.log(`Valid Package scopes:`, packages());
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': [2, 'always', packages()],
+    'scope-case': [2, 'always', 'kebab-case'],
+  },
+};
