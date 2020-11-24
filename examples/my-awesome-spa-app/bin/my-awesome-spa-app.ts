a9d5e3e63fce25c7c5db393870364fba05d04a1d
whimzyLive <rp970.patel@gmail.com>
2020-11-24 15:16:54 +1030

    test: fix tests

--- examples/my-awesome-spa-app/bin/my-awesome-spa-app.ts
+++ examples/my-awesome-spa-app/bin/my-awesome-spa-app.ts
@@ -2,6 +2,7 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { MyAwesomeSpaAppStack } from '../lib/my-awesome-spa-app-stack';
+import { SubscriptionStack } from '../lib/subscription-stack';
 
 const app = new cdk.App();
 new MyAwesomeSpaAppStack(app, 'MyAwesomeSpaAppStack', {
@@ -12,5 +13,11 @@ new MyAwesomeSpaAppStack(app, 'MyAwesomeSpaAppStack', {
   webAliases: ['my-app.domain.com'],
   zoneId: '1231241231',
   zoneName: 'domain.com',
-  certificateArn: 'arn:certificate',
+  certificateArn: 'arn:aws:acm:us-east-1:00000012313:certificate/some-id',
+});
+
+new SubscriptionStack(app, 'MySubscriptionStack', {
+  env: {
+    region: 'ap-southeast-2',
+  },
 });
